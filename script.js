a10c1a1965625d9c722e132eb4368c1275317a81
Zibi95 <sylwester.sob95@gmail.com>
2022-10-04 07:55:58 +0200

    Add board where you can color on hover add button to change grid size add color picker

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+// Elements
+const board = document.querySelector(".board");
+const sizeBtn = document.querySelector(".size-btn");
+const colorPicker = document.querySelector(".color-picker");
 
+//function to create divs inside board 16x16
+const createGrid = function (size) {
+  for (row = 0; row < size; row++) {
+    for (col = 0; col < size; col++) {
+      const tile = document.createElement("div");
+      board.appendChild(tile);
+    }
+  }
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+};
+
+const colorGrid = function (e) {
+  if (e.target === board) return;
+  const color = colorPicker.value;
+  e.target.style.backgroundColor = color;
+};
+
+const changeGridSize = function () {
+  const size = prompt("How many squares do You wish?");
+  if (16 > +size || !size || 100 < +size) return;
+  board.textContent = "";
+  createGrid(size);
+};
+
+board.addEventListener("mouseover", colorGrid);
+sizeBtn.addEventListener("click", changeGridSize);
